31b5ff43aca37881b13d96651921ad2ae36a40ab
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-08-02 11:46:10 -0600

    log transformations and unit tests

--- lib/utils.py
+++ lib/utils.py
@@ -11,3 +11,15 @@ def get_spark_app_config():
         spark_conf.set(key,val)
 
     return spark_conf
+
+def load_survey_df(spark, data_file):
+
+    return spark.read\
+            .option("header", "true") \
+            .option("inferSchema", "true") \
+            .csv(data_file)
+
+def count_by_country(survey_df):
+    return survey_df.where("Age < 40") \
+        .select("Age", "Gender", "Country", "state") \
+        .groupBy("Country").count()
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from pyspark import SparkConf
 from pyspark.sql import *
 from lib.logger import log4J
+import sys
 
-from lib.utils import get_spark_app_config
+from lib.utils import get_spark_app_config, load_survey_df, count_by_country
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -20,14 +21,33 @@ if __name__ == '__main__':
 
     logger = log4J(spark)
 
+    #Log an error if filename not available
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
+
     logger.info("Starting HelloSpark")
     #any processing code
 
-    conf_out = spark.sparkContext.getConf() #Read spark configs
-    logger.info(conf_out.toDebugString())
+    # Read spark configs
+    #conf_out = spark.sparkContext.getConf()
+    #logger.info(conf_out.toDebugString())
 
-    logger.info("Finished HelloSpark")
+    survey_df = load_survey_df(spark, sys.argv[1])
 
+    #repartition the dataframe
+    partitioned_survey_df = survey_df.repartition(2)
+
+    #spark transformations
+    transformed_survey_df = count_by_country(partitioned_survey_df)
+
+    logger.info(transformed_survey_df.collect())
+
+    transformed_survey_df.show()
+
+    logger.info("  Finished HelloSpark")
+
+    input("Press Enter")
     #Stop driver after work is done
     spark.stop()
 
