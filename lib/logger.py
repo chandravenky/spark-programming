14e594df4e62ff936a6d04d5852b5a31f185a600
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-08-06 17:47:06 -0600

    sparkRDD

--- lib/logger.py
+++ lib/logger.py
@@ -1,10 +1,12 @@
-class log4J:
+class Log4j:
     def __init__(self, spark):
-        log4j = spark._jvm.org.apache.log4j #get log4J instance from spark session
+        log4j = spark._jvm.org.apache.log4j
+
         root_class = "guru.learningjournal.spark.examples"
         conf = spark.sparkContext.getConf()
         app_name = conf.get("spark.app.name")
-        self.logger = log4j.LogManager.getLogger(root_class + "." + app_name) #define a logger attribute
+
+        self.logger = log4j.LogManager.getLogger(root_class + "." + app_name)
 
     def warn(self, message):
         self.logger.warn(message)
--- main.py
+++ main.py
@@ -1,35 +1,13 @@
-from pyspark import SparkConf
-from pyspark.sql import *
-from lib.logger import log4J
 
-from lib.utils import get_spark_app_config
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print_hi('PyCharm')
 
-    #Create driver
-    # Start a spark session
-    # Set configuration
-    # Local multi-threaded architecture with 3 threads
-
-    conf = get_spark_app_config()
-
-    spark = SparkSession.builder \
-            .config(conf=conf) \
-            .getOrCreate()
-
-    logger = log4J(spark)
-
-    logger.info("Starting HelloSpark")
-    #any processing code
-
-    conf_out = spark.sparkContext.getConf() #Read spark configs
-    logger.info(conf_out.toDebugString())
-
-    logger.info("Finished HelloSpark")
-
-    #Stop driver after work is done
-    spark.stop()
-
-    print('Hello Spark')
-
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
